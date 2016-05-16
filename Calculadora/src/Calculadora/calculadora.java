8abc797f7e89569a1ece7aff0541e4ba5c0c412d
KarolIvette <karolcedeno95@gmail.com>
2016-05-16 11:01:20 -0500

    CLASE CALCULADORA

---
+++ Calculadora/src/Calculadora/calculadora.java
@@ -0,0 +1,54 @@
+package Calculadora;
+import javax.swing.JOptionPane;
+
+public class calculadora {
+	float num1;
+	float num2;
+
+    public calculadora(float num1, float num2){
+    	this.num1 = num1;
+    	this.num2 = num2;
+    }
+	
+	public float get_num1(){
+		return num1; 
+	}
+	
+	public void set_num1(float num1){
+		this.num1 =  num1 ; 
+	}
+	
+	public float get_num2(){
+		return num2; 
+	}
+	
+	public void set_num2(float num2){
+		this.num2 =  num2 ; 
+	}
+	
+	public void suma(){
+		float suma = this.num1 + this.num2;
+		JOptionPane.showMessageDialog(null, "LA SUMA ES : "+Float.toString(suma));
+	}
+	
+	public void resta(){
+		float resta = this.num1 - this.num2;
+		JOptionPane.showMessageDialog(null, "LA RESTA ES : "+Float.toString(resta));
+	}
+	
+	public void multiplicacion(){
+		float multiplicacion = this.num1 * this.num2;
+		JOptionPane.showMessageDialog(null, "LA MULTIPLICACION ES : "+Float.toString(multiplicacion));
+	}
+	
+	public void division(){
+		float division = this.num1 / this.num2;
+		JOptionPane.showMessageDialog(null, "LA DIVISION ES : "+Float.toString(division));
+	}
+	
+	public void raizcuadrada(float num){
+		float raiz = (float) Math.sqrt(num);
+		JOptionPane.showMessageDialog(null, "LA RAIZ CUADRADA DE : "
+		+Float.toString(num)+" es = "+Float.toString(raiz));
+	}
+}
---
+++ Calculadora/src/Calculadora/main.java
@@ -0,0 +1,23 @@
+package Calculadora;
+import javax.swing.JOptionPane;
+
+public class main {
+	public static void main(String[] args) 
+	{
+		float num1;
+		float num2;
+		
+		num1 = Float.parseFloat(JOptionPane.showInputDialog(null, "INGRESE EL PRIMER NUMERO:"));
+		num2 = Float.parseFloat(JOptionPane.showInputDialog(null, "INGRESE EL SEGUNDO NUMERO:"));
+		
+		calculadora cal = new calculadora(num1, num2);
+		
+		cal.suma();
+		cal.resta();
+		cal.multiplicacion();
+		cal.division();
+		
+		cal.raizcuadrada(cal.get_num1());
+		cal.raizcuadrada(cal.get_num2());
+	}
+}
